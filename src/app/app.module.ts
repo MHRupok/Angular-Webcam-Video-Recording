95c927a5245dc13ba2d75c0070a7092dcbb2ac49
RupokGT <mehedi@gigatechltd.com>
2020-10-28 17:43:00 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VideojsRecordComponent } from './videojs-record/videojs-record.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideojsRecordComponent
   ],
   imports: [
     BrowserModule,
